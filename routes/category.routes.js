2391800ef01b33b94bfb49324b5f24432260c4ab
cemil <cemil@cemil.cemil>
2023-11-08 01:05:02 +0400

    Add cities

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -4,5 +4,6 @@ const categoryController = require('./../controllers/categoryController');
 const router = express.Router();
 
 router.get('/getCategories', categoryController.getCategories);
+router.get('/getCities', categoryController.getCities);
 
 module.exports = router;
